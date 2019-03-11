bf33cb1745c70034e4202b00904bdeaf98ec0c62
Angel Flores <aflores1@drew.edu>
2019-03-11 03:22:34 -0400

    imported csv through python file

---
+++ free_time.py
@@ -0,0 +1,5 @@
+import csv
+with open('CourseList.FA18.09.06.18.csv', 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        print ' '.join(row)
