7de1dd4ab1bbf97f4f13b44eba755ce3c8f13aba
gradovic <gradovic@drew.edu>
2019-03-12 03:00:48 -0400

    f

--- free_time.py
+++ free_time.py
@@ -32,21 +32,22 @@ with open('CourseList2.FA18.09.06.18.csv', "rb") as file:
 with open('StudentsandCourses2.FA18.09.06.18.csv', "rb") as file:
    reader = csv.reader(file)
    ClassLabel = "ITAL 201"
-   ImportantClasses = ""
+   ImportantClasses = []
 
    # r and c tell us where to grid the labels
    r = 0
    for col in reader:
       c = 0
       for row in col:
-         # i've added some styling
-        label = tkinter.Label(root, width = 10, height = 2, \
-                               text = row, relief = tkinter.RIDGE,)
-        label.grid(row = r, column = c)
-        if c == 0:
-           if ClassLabel in row:
-              ImportantClasses += row
-        c += 1
+         if c == 0:
+            # i've added some styling
+           label = tkinter.Label(root, width = 10, height = 2, \
+                                  text = row, relief = tkinter.RIDGE,)
+           label.grid(row = r, column = c)
+           if c == 0:
+              if ClassLabel in row:
+                 ImportantClasses.append(row)
+           c += 1
       r += 1
 
 print (ImportantClasses)
