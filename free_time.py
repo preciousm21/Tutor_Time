9ee4f60ba1e96087b61ed523997b9d5e3546df23
gradovic <gradovic@drew.edu>
2019-03-12 01:32:14 -0400

    started working with StudentsandCourses

--- free_time.py
+++ free_time.py
@@ -24,11 +24,31 @@ with open('CourseList2.FA18.09.06.18.csv', "rb") as file:
            ClassLabel += row
         if c == 4:
            ClassLabel += row
-           print (ClassLabel)
+#           print (ClassLabel)
         c += 1
       r += 1
 
 
+with open('StudentsandCourses2.FA18.09.06.18.csv', "rb") as file:
+   reader = csv.reader(file)
+   ClassLabel = "ITAL 201"
+   ImportantClasses = ""
 
+   # r and c tell us where to grid the labels
+   r = 0
+   for col in reader:
+      c = 0
+      for row in col:
+         # i've added some styling
+        label = tkinter.Label(root, width = 10, height = 2, \
+                               text = row, relief = tkinter.RIDGE,)
+        label.grid(row = r, column = c)
+        if c == 0:
+           if row == ClassLabel:
+              ImportantClasses += row
+        c += 1
+      r += 1
+
+print (ImportantClasses)
 root.title("Tutor Time")
 root.mainloop()
