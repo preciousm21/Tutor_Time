a13ea36f7b7301fd9d7e72ccd737788bb3d06fb3
gradovic <gradovic@drew.edu>
2019-03-12 03:57:28 -0400

    got the courses that come after the chosen course

--- Sample_python.py
+++ Sample_python.py
@@ -6,12 +6,20 @@ root = Tk()
 
 X=[]
 list_need = []
-keyword = "MUS 326"
+keyword = "MATH 325"
+marker = 0
 
 with open('StudentsandCourses.FA18.09.06.18.csv', 'r') as infile:    
    for line in infile:
+      if marker == 1:
+         if "Total For" not in line:
+            list_need.append(line)
+         else:
+            marker = 0
       if keyword in line:
           list_need.append(line)
+          marker = 1
+          
       
       # Split on comma first
       cols = [x.strip() for x in line.split(',')]
@@ -25,16 +33,11 @@ with open('StudentsandCourses.FA18.09.06.18.csv', 'r') as infile:
          if word not in X:
             X.append(word)
 
-<<<<<<< HEAD
-for w in X:
-   print (w)
-=======
 #for w in X:
 #  print w
 
 print(list_need)
 msg = tkinter.Message(root, text=list_need)
-msg.config(bg='lightgreen', font=('times', 24, 'italic'))
+msg.config(bg='lightgreen', font=('times', 12, 'italic'))
 msg.pack()
 tkinter.mainloop()
->>>>>>> e39172b17bb38932486777508c7741292c3ebb9f
