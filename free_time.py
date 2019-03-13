5ab9aedad0d704cbb3402ce6c6a705d10af8e722
preciousm21 <pmartinez@drew.edu>
2019-03-13 01:20:25 -0400

    self, master and main added

--- free_time.py
+++ free_time.py
@@ -5,34 +5,34 @@ import ttk
 
 from ttk import *
 
+#class App(Tk.frame):
+ #  def __init__(self, master):
+  #    self.master=master
+   #   frame = Frame(master)
+    #  frame.pack()
 
+      # open file
+      with open('CourseList2.FA18.09.06.18.csv', "rb") as file:
+         reader = csv.reader(file)
 
-
-root = Tk()
-
-
-# open file
-with open('CourseList2.FA18.09.06.18.csv', "rb") as file:
-   reader = csv.reader(file)
-
-   # r and c tell us where to grid the labels
-   r = 0
-   for col in reader:
-      c = 0
-      for row in col:
-         # i've added some styling
-        label = tkinter.Label(root, width = 10, height = 2, \
-                               text = row, relief = tkinter.RIDGE,)
-        label.grid(row = r, column = c)
-        if c == 2:
-           ClassLabel = ""
-        if c == 3:
-           ClassLabel += row
-        if c == 4:
-           ClassLabel += row
-#           print (ClassLabel)
-        c += 1
-      r += 1
+         # r and c tell us where to grid the labels
+         r = 0
+         for col in reader:
+            c = 0
+         for row in col:
+               # i've added some styling
+            label = tkinter.Label(root, width = 10, height = 2, \
+                                    text = row, relief = tkinter.RIDGE,)
+            label.grid(row = r, column = c)
+            if c == 2:
+               ClassLabel = ""
+            if c == 3:
+               ClassLabel += row
+            if c == 4:
+               ClassLabel += row
+      #           print (ClassLabel)
+            c += 1
+            r += 1
 
 
 with open('StudentsandCourses2.FA18.09.06.18.csv', "rb") as file:
@@ -56,7 +56,16 @@ with open('StudentsandCourses2.FA18.09.06.18.csv', "rb") as file:
                  ImportantClasses.append(row)
            c += 1
       r += 1
-
 print (ImportantClasses)
-root.title("Tutor Time")
-root.mainloop()
+
+#run main loop
+#def main(): 
+ #   root = Tk()
+  #  App = Demo1(root)
+   # root.title("Tutor Time")
+    #root.mainloop()
+
+#if __name__ == '__main__':
+    main()
+
+
